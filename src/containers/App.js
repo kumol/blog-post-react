8ef5174b12281e5ec52288e460df814ef38b4c67
kumol <kumol01763@gmail.com>
2021-04-28 02:25:47 +0600

    React Http handler project structure

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Blog from './Blog/Blog';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Blog />
+      </div>
+    );
+  }
+}
+
+export default App;
