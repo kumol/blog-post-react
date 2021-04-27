abdb7bdc385dc158e8b72035194f19f6abfc41ef
kumol <kumol01763@gmail.com>
2021-04-28 03:18:09 +0600

    getting group of post and getting post by id

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state={
+        posts:[],
+        selectedPost:null
+    }
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/posts").then(response=>{
+            let posts = response.data.slice(0,5);
+            const updatedPost = posts.map(post=>{
+                return{
+                    ...post,
+                    auther:"Max"
+                }
+            });
+            this.setState({posts:updatedPost});
+        });
+    }
+    selectedPostHandler(id){
+        this.setState({selectedPost:id});
+    }
     render () {
+        const posts = this.state.posts.map(post=>{
+            return <Post
+                title={post.title}
+                auther={post.auther}
+                key={post.id}
+                clicked={()=>this.selectedPostHandler(post.id)}
+            />
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPost}/>
                 </section>
                 <section>
                     <NewPost />
