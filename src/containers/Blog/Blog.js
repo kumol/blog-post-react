cad0803ad5081fe5c3fbb0d5d3c08d3705a1ab40
kumol <kumol01763@gmail.com>
2021-04-29 23:43:39 +0600

    router link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import {Route} from "react-router-dom";
+import {Link, Route} from "react-router-dom";
 import NewPost from './NewPost/NewPost';
 class Blog extends Component {
     
@@ -12,8 +12,8 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New post</Link></li>
                         </ul>
                     </nav>
                 </header>
